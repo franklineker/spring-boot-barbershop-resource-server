d96c4a08fbb47d49a4d9dc59b96922baeb355310
Frank Lineker <frankdebouer@hotmail.com>
2023-12-24 18:26:20 -0300

    connected to mongo atlas

--- src/main/java/br/com/drnavalha/DrnavalhaApplication.java
+++ src/main/java/br/com/drnavalha/DrnavalhaApplication.java
@@ -1,5 +1,10 @@
 package br.com.drnavalha;
 
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
---
+++ src/main/java/br/com/drnavalha/security/MongoConfig.java
@@ -0,0 +1,41 @@
+package br.com.drnavalha.security;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
+        MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                MongoDatabase database = mongoClient.getDatabase(getDatabaseName());
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+            return MongoClients.create(settings);
+        }
+    }
+}
